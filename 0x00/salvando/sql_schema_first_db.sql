6d1e2bd0d0ac74bccce4fa95dcfa331d0cc5a91d
Renan de Lima Gomes <renanlg@ciandt.com>
2022-06-09 12:42:57 -0300

    Export schema

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE CLIENTE (
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+nome VARCHAR(80) NOT NULL,
+email VARCHAR(40) NOT NULL,
+telefone int);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
