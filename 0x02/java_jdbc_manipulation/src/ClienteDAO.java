5c13d37f3ef66ac1fec0d73bb87e10a544d1716d
Renan de Lima Gomes <renanlg@ciandt.com>
2022-06-14 08:16:13 -0300

    JDBC - 0. Criando relacionamentos entre as classes Java com a tabela do Banco de Dados

---
+++ 0x02/java_jdbc_manipulation/src/ClienteDAO.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.sql.Connection;
+
+public interface ClienteDAO {
+    public Connection connect(String urlConexao);
+    public void createTable(String urlConexao);
+    public void insert(String urlConexao);
+    public void selectAll(String urlConexao);
+    public void update(String urlConexao, int id, String name, Integer idade);
+    public void delete(String urlConexao, int id);
+}
---
+++ 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
@@ -0,0 +1,35 @@
+package src;
+
+import java.sql.Connection;
+
+public class ClienteDAOImpl implements ClienteDAO {
+    @Override
+    public Connection connect(String urlConexao) {
+        return null;
+    }
+
+    @Override
+    public void createTable(String urlConexao) {
+
+    }
+
+    @Override
+    public void insert(String urlConexao) {
+
+    }
+
+    @Override
+    public void selectAll(String urlConexao) {
+
+    }
+
+    @Override
+    public void update(String urlConexao, int id, String name, Integer idade) {
+
+    }
+
+    @Override
+    public void delete(String urlConexao, int id) {
+
+    }
+}
